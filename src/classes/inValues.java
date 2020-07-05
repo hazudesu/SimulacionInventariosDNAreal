29c4606d0a031464a45271f0a54cd7163c5977dd
hazudesu <46248038+hazudesu@users.noreply.github.com>
2020-07-04 23:12:18 -0400

    avance
    cosas

--- src/classes/inValues.java
+++ src/classes/inValues.java
@@ -1,4 +1,19 @@
 package classes;
 
 public class inValues {
+    char timeUnit;
+    int timeAmount;
+    int demandValues;
+    int demand;
+    int demandProb;
+    int deliverTimeAmount;
+    int deliverTime;
+    int deliverTimeProb;
+    int w8TimeAmntClient;
+    int clientW8TimeArt;
+    int w8TimeClientProb;
+    int invCost;
+    int purchaseCost;
+    int orderCost;
+
 }
