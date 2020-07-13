cba58b7607f8b8652debf5b2b03442602c32dc9d
NestorAngeles <nestorivan.27@gmail.com>
2020-07-12 20:40:09 -0400

    Avances

---
+++ src/interfaces/tabla.java
@@ -0,0 +1,32 @@
+package interfaces;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class tabla extends JFrame {
+
+    JTable tabla;
+
+    public tabla(){
+        setLayout(new FlowLayout());
+
+        String [] columnName = {"Dia", "Inv. Inic.", "No. A Demanda","Demanda","Inv. Final","Inv. Prom","Faltante","No. Orden","No. A  tiempo de entrega",
+                "Tiempo de Entrega", "No. A espera","Tiempo de Espera"};
+
+        Object[][] data = {
+                {"Bill","Gates","20","Bill","Gates","20","Bill","Gates","20","Bill","Gates","20"},
+                {"Bill","Gates","20","Bill","Gates","20","Bill","Gates","20","Bill","Gates","20"},
+                {"Bill","Gates","20","Bill","Gates","20","Bill","Gates","20","Bill","Gates","20"},
+                //{"Bill","Gates","20"},
+        };
+
+        tabla = new JTable(data,columnName);
+        tabla.setPreferredScrollableViewportSize(new Dimension(500,50));
+        tabla.setFillsViewportHeight(true);
+
+        JScrollPane scrollPane = new JScrollPane(tabla);
+        add(scrollPane);
+    }
+
+
+}
