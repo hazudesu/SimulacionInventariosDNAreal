49976ff529c320afcc69c713a47a75d0fcf9521a
hazudesu <46248038+hazudesu@users.noreply.github.com>
2020-07-15 00:31:03 -0400

    epa

--- src/interfaces/Interfaz_Simulacion.java
+++ src/interfaces/Interfaz_Simulacion.java
@@ -926,6 +926,7 @@ public class Interfaz_Simulacion extends javax.swing.JFrame {
 
 
 
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
