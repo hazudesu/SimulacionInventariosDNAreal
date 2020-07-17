a740214d8c9fe125763f41aafa621817d6820c87
hazudesu <46248038+hazudesu@users.noreply.github.com>
2020-07-17 19:19:16 -0400

    update

--- src/interfaces/Interfaz_Simulacion.java
+++ src/interfaces/Interfaz_Simulacion.java
@@ -350,7 +350,11 @@ public class Interfaz_Simulacion extends javax.swing.JFrame {
         jButton1.setText("Cargar Archivo");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                try {
+                    jButton1ActionPerformed(evt);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
